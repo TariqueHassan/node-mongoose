a2379b2791e071ec83e8cd4c179429c5be39f3df
TariqueHassan <tariquehassan.cs17@iba-suk.edu.pk>
2021-08-09 16:05:35 +0500

    Mongoose Part 2

--- index.js
+++ index.js
@@ -8,26 +8,59 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log('Connected Correctly to server');
 
-    var newDish = Dishes({
+    Dishes.create({
         name: 'Uthappizza (mongoose)',
         description: 'test (mongoose)'
-    });
+    })
+    .then((dish) => {
+        console.log("Dish: "+dish);
 
-    newDish.save()
-        .then((dish) => {
-            console.log("Dish: "+dish);
+        Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log("Dishes: "+dishes);
 
-            Dishes.find({}).exec();
-        })
-        .then((dishes) => {
-            console.log("Dishes: "+dishes);
-
-            return Dishes.deleteMany({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+        return Dishes.deleteMany({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 });
+
+
+// const mongoose = require('mongoose');
+
+// const Dishes = require('./models/dishes');
+
+// const url = 'mongodb://localhost:27017/conFusion';
+// const connect = mongoose.connect(url);
+
+// connect.then((db) => {
+//     console.log('Connected Correctly to server');
+
+//     var newDish = Dishes({
+//         name: 'Uthappizza (mongoose)',
+//         description: 'test (mongoose)'
+//     });
+
+//     newDish.save()
+//         .then((dish) => {
+//             console.log("Dish: "+dish);
+
+//             Dishes.find({}).exec();
+//         })
+//         .then((dishes) => {
+//             console.log("Dishes: "+dishes);
+
+//             return Dishes.deleteMany({});
+//         })
+//         .then(() => {
+//             return mongoose.connection.close();
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         })
+// });
