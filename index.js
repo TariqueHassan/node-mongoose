6aa1233a022eb4106e82d4b80db08b75df3e38bd
TariqueHassan <tariquehassan.cs17@iba-suk.edu.pk>
2021-08-09 15:46:30 +0500

    Mongoose Part 1

---
+++ index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+    console.log('Connected Correctly to server');
+
+    var newDish = Dishes({
+        name: 'Uthappizza (mongoose)',
+        description: 'test (mongoose)'
+    });
+
+    newDish.save()
+        .then((dish) => {
+            console.log("Dish: "+dish);
+
+            Dishes.find({}).exec();
+        })
+        .then((dishes) => {
+            console.log("Dishes: "+dishes);
+
+            return Dishes.deleteMany({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+});
